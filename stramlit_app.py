d028fa9edb6d23229b66a7ce3dca8f55c74d5fc0
Srameegy <143444877+Srameegy@users.noreply.github.com>
2024-06-12 13:17:54 +0300

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 from snowflake.snowpark.functions import col
 import requests
+import Pandas
 
 
 # Write directly to the app
@@ -22,7 +23,11 @@ session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
+
 
 ingredients_list = st.multiselect(
 'Choose up to 5 Ingredients:'
