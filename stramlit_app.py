a8757ff50b1d3a6f813ba714a48e98c6b4dd64fd
Srameegy <143444877+Srameegy@users.noreply.github.com>
2024-06-12 12:57:40 +0300

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -51,5 +51,6 @@ if ingredients_list:
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.JSON())
+st.text(fruityvice_response.json())
+fv_df =st.datadrame(data=fruityvice_response.json(),use_container_width=True)    
 
