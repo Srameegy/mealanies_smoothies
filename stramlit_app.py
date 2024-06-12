488d95c0e8700d87c71078e45a90392b3382380c
Srameegy <143444877+Srameegy@users.noreply.github.com>
2024-06-12 13:00:34 +0300

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 from snowflake.snowpark.functions import col
-
+import requests
 
 
 # Write directly to the app
@@ -49,7 +49,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
 fv_df =st.dataframe(data=fruityvice_response.json(),use_container_width=True)    
